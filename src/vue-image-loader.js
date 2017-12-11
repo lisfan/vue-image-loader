bd7f98d40772e2ed0e96e8450b352a4e9adebce4
lisfan <goolisfan@gmail.com>
2017-12-11 15:03:05 +0800

    docs: 更新@returns -> @return

---
+++ src/vue-image-loader.js
@@ -0,0 +1,24 @@
+/**
+ * @file 图片加载器
+ * @author lisfan <goolisfan@gmail.com>
+ * @version 1.3.0
+ * @licence MIT
+ */
+import EventQueues from '@~lisfan/event-queues'
+
+// base64格式匹配正则表达式
+const BASE64_REG = /data:(.*);base64,/
+
+// vue图片加载器指令类
+class VueImageLoaderDirector {
+
+  imageComplete
+  imageComplete
+  currentImgSrc
+  isRequestPHImage
+  constructor(options){
+
+  }
+}
+
+export default ImageLoader
